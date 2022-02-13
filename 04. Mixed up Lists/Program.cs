768493bb59b721536f6c905b43772438052b3d09
Любомир Лалов <ldlalov@hotmail.com>
2022-02-13 18:25:20 +0200

    Ready

--- 04. Mixed up Lists/Program.cs
+++ 04. Mixed up Lists/Program.cs
@@ -8,9 +8,45 @@ namespace _04._Mixed_up_Lists
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>();
+            List<int> result = new List<int>();
             List<int> firstList = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
             List<int> secondList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            secondList.Reverse();
+            List<int> range = new List<int>();
+            int shorterList = 0;
+            if (firstList.Count < secondList.Count)
+            {
+                shorterList = firstList.Count;
+                for (int i = 2; i >= 1; i--)
+                {
+                    range.Add(secondList.Last());
+                    secondList.RemoveAt(secondList.Count - 1);
+                }
+
+            }
+            else
+            {
+                shorterList = secondList.Count;
+                for (int i = 2; i >= 1; i--)
+                {
+                    range.Add(firstList.Last());
+                    firstList.RemoveAt(firstList.Count-1);
+                }
+            }
+            range.Sort();
+            for (int i = 0; i < shorterList; i++)
+            {
+                if (firstList[i]>range[0] && firstList[i]<range[1])
+                {
+                result.Add(firstList[i]);
+                }
+                if (secondList[i]>range[0] && secondList[i]<range[1])
+                {
+                    result.Add(secondList[i]);
+                }
+            }
+            result.Sort();
+            Console.WriteLine(string.Join(' ',result));
         }
     }
 }
---
+++ 05. Drum Set/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _05._Drum_Set
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double savings = double.Parse(Console.ReadLine());
+            List<int> drums = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            string command = "";
+            List<int> drumsAsNew = drums.ToList();
+            while ((command = Console.ReadLine()) != "Hit it again, Gabsy!")
+            {
+                int hitPower = int.Parse(command);
+                for (int i = 0;i<drums.Count;i++)
+                {
+                    drums[i] -= hitPower;
+                    if (drums[i] <= 0)
+                    {
+                        if (savings>= drumsAsNew[i] * 3)
+                        {
+                            drums[i] = drumsAsNew[i];
+                            savings -= drumsAsNew[i] * 3;
+                        }
+                        else
+                        {
+                            drums.RemoveAt(i);
+                            drumsAsNew.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(' ', drums));
+            Console.WriteLine($"Gabsy has {savings:f2}lv.");
+        }
+    }
+}
